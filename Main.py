a167d6316d4152353d61d032be49ad6365fec763
VisionR1 <25982450+VisionR1@users.noreply.github.com>
2025-03-17 15:07:42 +0200

    Update Main.py
    - Delete submenu (XDA and GitHub).
    - XDA new Links.
    - Proper icons for GitHub and XDA Links.
    - Remove https://github.com/TheFreeman193/PIFS because it is abandoned.

--- Main.py
+++ Main.py
@@ -1874,68 +1874,55 @@ class PixelFlasher(wx.Frame):
         # separator
         help_menu.AppendSeparator()
         # Links Submenu
-        links = wx.Menu()
-        github = wx.Menu()
-        xda = wx.Menu()
-        xda_guides_category = wx.Menu()
-        xda_general_category = wx.Menu()
-        xda_submenu_item = links.AppendSubMenu(xda, "XDA Community (Forum)")
-        xda_submenu_item.SetBitmap(images.forum_24.GetBitmap())
-        xda_general_item = xda.AppendSubMenu(xda_general_category, "General")
-        xda_general_item.SetBitmap(images.about_24.GetBitmap())
-        self.linksMenuItem16 = xda_general_category.Append(wx.ID_ANY, "osm0sis\'s PIF FAQ")
-        self.linksMenuItem17 = xda_general_category.Append(wx.ID_ANY, "V0latyle\'s PI API Info")
-        self.linksMenuItem18 = xda_general_category.Append(wx.ID_ANY, "chiteroman\'s PlayIntegrityFix")
-        self.linksMenuItem19 = xda_general_category.Append(wx.ID_ANY, "Tricky Store (Support Thread)")
-        xda_guides_item = xda.AppendSubMenu(xda_guides_category, "Guides")
-        xda_guides_item.SetBitmap(images.guide_24.GetBitmap())
-        self.linksMenuItem1 = xda_guides_category.Append(wx.ID_ANY, "Homeboy76\'s Guide")
-        self.linksMenuItem2 = xda_guides_category.Append(wx.ID_ANY, "V0latyle\'s Guide")
-        self.linksMenuItem3 = xda_guides_category.Append(wx.ID_ANY, "roirraW\'s Guide")
+        links = wx.Menu()       
+        self.linksMenuItem15 = links.Append(wx.ID_ANY, "osm0sis\'s PIF FAQ")
+        self.linksMenuItem16 = links.Append(wx.ID_ANY, "V0latyle\'s PI API Info")
+        self.linksMenuItem17 = links.Append(wx.ID_ANY, "chiteroman\'s PlayIntegrityFix")
+        self.linksMenuItem18 = links.Append(wx.ID_ANY, "Tricky Store (Support Thread)")
         links.AppendSeparator()
-        github_submenu_item = links.AppendSubMenu(github, "GitHub")
-        github_submenu_item.SetBitmap(images.github_24.GetBitmap())
-        self.linksMenuItem4 = github.Append(wx.ID_ANY, "osm0sis\'s PlayIntegrityFork")
-        self.linksMenuItem5 = github.Append(wx.ID_ANY, "chiteroman\'s PlayIntegrityFix")
-        self.linksMenuItem15 = github.Append(wx.ID_ANY, "5ec1cff\'s TrickyStore")
-        self.linksMenuItem8 = github.Append(wx.ID_ANY, "TheFreeman193\'s Play Integrity Fix Props Collection")
+        self.linksMenuItem1 = links.Append(wx.ID_ANY, "Homeboy76\'s Guide")
+        self.linksMenuItem2 = links.Append(wx.ID_ANY, "V0latyle\'s Guide")
+        self.linksMenuItem3 = links.Append(wx.ID_ANY, "roirraW\'s Guide")
+        links.AppendSeparator()
+        self.linksMenuItem4 = links.Append(wx.ID_ANY, "osm0sis\'s PlayIntegrityFork")
+        self.linksMenuItem5 = links.Append(wx.ID_ANY, "chiteroman\'s PlayIntegrityFix")
+        self.linksMenuItem14 = links.Append(wx.ID_ANY, "5ec1cff\'s TrickyStore")
         links.AppendSeparator()
         self.linksMenuItem6 = links.Append(wx.ID_ANY, "Get the Google USB Driver")
         self.linksMenuItem7 = links.Append(wx.ID_ANY, "Android Security Update Bulletins")
         links.AppendSeparator()
-        self.linksMenuItem9 = links.Append(wx.ID_ANY, "Full OTA Images for Pixel Phones / Tablets")
-        self.linksMenuItem10 = links.Append(wx.ID_ANY, "Factory Images for Pixel Phones / Tablets")
-        self.linksMenuItem11 = links.Append(wx.ID_ANY, "Full OTA Images for Pixel Watches")
-        self.linksMenuItem12 = links.Append(wx.ID_ANY, "Factory Images for Pixel Watches")
+        self.linksMenuItem8 = links.Append(wx.ID_ANY, "Full OTA Images for Pixel Phones / Tablets")
+        self.linksMenuItem9 = links.Append(wx.ID_ANY, "Factory Images for Pixel Phones / Tablets")
+        self.linksMenuItem10 = links.Append(wx.ID_ANY, "Full OTA Images for Pixel Watches")
+        self.linksMenuItem11 = links.Append(wx.ID_ANY, "Factory Images for Pixel Watches")
         links.AppendSeparator()
-        self.linksMenuItem13 = links.Append(wx.ID_ANY, "Full OTA Images for Pixel Beta 15")
-        self.linksMenuItem14 = links.Append(wx.ID_ANY, "Factory Images for Pixel Beta 15")
+        self.linksMenuItem12 = links.Append(wx.ID_ANY, "Full OTA Images for Pixel Beta 15")
+        self.linksMenuItem13 = links.Append(wx.ID_ANY, "Factory Images for Pixel Beta 15")
         self.linksMenuItem1.SetBitmap(images.guide_24.GetBitmap())
         self.linksMenuItem2.SetBitmap(images.guide_24.GetBitmap())
         self.linksMenuItem3.SetBitmap(images.guide_24.GetBitmap())
-        self.linksMenuItem4.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem5.SetBitmap(images.open_link_24.GetBitmap())
+        self.linksMenuItem4.SetBitmap(images.github_24.GetBitmap())
+        self.linksMenuItem5.SetBitmap(images.github_24.GetBitmap())
         self.linksMenuItem6.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem7.SetBitmap(images.open_link_24.GetBitmap())
+        self.linksMenuItem7.SetBitmap(images.open_link_24.GetBitmap())       
         self.linksMenuItem8.SetBitmap(images.open_link_24.GetBitmap())
         self.linksMenuItem9.SetBitmap(images.open_link_24.GetBitmap())
         self.linksMenuItem10.SetBitmap(images.open_link_24.GetBitmap())
         self.linksMenuItem11.SetBitmap(images.open_link_24.GetBitmap())
         self.linksMenuItem12.SetBitmap(images.open_link_24.GetBitmap())
         self.linksMenuItem13.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem14.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem15.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem16.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem17.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem18.SetBitmap(images.open_link_24.GetBitmap())
-        self.linksMenuItem19.SetBitmap(images.open_link_24.GetBitmap())
+        self.linksMenuItem14.SetBitmap(images.github_24.GetBitmap())
+        self.linksMenuItem15.SetBitmap(images.forum_24.GetBitmap())
+        self.linksMenuItem16.SetBitmap(images.forum_24.GetBitmap())
+        self.linksMenuItem17.SetBitmap(images.forum_24.GetBitmap())
+        self.linksMenuItem18.SetBitmap(images.forum_24.GetBitmap())
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem1)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem2)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem3)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem4)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem5)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem6)
-        self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem7)
+        self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem7)       
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem8)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem9)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem10)
@@ -1947,7 +1934,6 @@ class PixelFlasher(wx.Frame):
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem16)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem17)
         self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem18)
-        self.Bind(wx.EVT_MENU, self._on_link_clicked, self.linksMenuItem19)
         links_item = help_menu.Append(wx.ID_ANY, 'Links', links)
         links_item.SetBitmap(images.open_link_24.GetBitmap())
         # separator
@@ -2337,18 +2323,17 @@ _If you have selected multiple APKs to install, the options will apply to all AP
                 self.linksMenuItem5.GetId(): ('https://github.com/chiteroman/PlayIntegrityFix', "chiteroman's PlayIntegrityFix"),
                 self.linksMenuItem6.GetId(): ('https://developer.android.com/studio/run/win-usb?authuser=1%2F', "Get the Google USB Driver"),
                 self.linksMenuItem7.GetId(): ('https://source.android.com/docs/security/bulletin/', "Android Security Update Bulletins"),
-                self.linksMenuItem8.GetId(): ('https://github.com/TheFreeman193/PIFS', "TheFreeman193's Play Integrity Fix Props Collection"),
-                self.linksMenuItem9.GetId(): (FULL_OTA_IMAGES_FOR_PIXEL_DEVICES, "Full OTA Images for Pixel Phones, Tablets"),
-                self.linksMenuItem10.GetId(): (FACTORY_IMAGES_FOR_PIXEL_DEVICES, "Factory Images for Pixel Phones, Tablets"),
-                self.linksMenuItem11.GetId(): (FULL_OTA_IMAGES_FOR_WATCH_DEVICES, "Full OTA Images for Pixel Watches"),
-                self.linksMenuItem12.GetId(): (FACTORY_IMAGES_FOR_WATCH_DEVICES, "Factory Images for Pixel Watches"),
-                self.linksMenuItem13.GetId(): (FULL_OTA_IMAGES_FOR_BETA, "Full OTA Images for Pixel Beta 15"),
-                self.linksMenuItem14.GetId(): (FACTORY_IMAGES_FOR_BETA, "Factory Images for Pixel Beta 15"),
-                self.linksMenuItem15.GetId(): ('https://github.com/5ec1cff/TrickyStore', "5ec1cff's TrickyStore"),
-                self.linksMenuItem16.GetId(): ('https://xdaforums.com/t/pif-faq.4653307/', "osm0sis's PIF FAQ"),
-                self.linksMenuItem17.GetId(): ('https://xdaforums.com/t/info-play-integrity-api-replacement-for-safetynet.4479337/', "V0latyle\'s PI API Info"),
-                self.linksMenuItem18.GetId(): ('https://xdaforums.com/t/module-play-integrity-fix-safetynet-fix.4607985/', "chiteroman's PlayIntegrityFix"),
-                self.linksMenuItem19.GetId(): ('https://xdaforums.com/t/tricky-store-bootloader-keybox-spoofing.4683446/', "Tricky Store (Support Thread)"),
+                self.linksMenuItem8.GetId(): (FULL_OTA_IMAGES_FOR_PIXEL_DEVICES, "Full OTA Images for Pixel Phones, Tablets"),
+                self.linksMenuItem9.GetId(): (FACTORY_IMAGES_FOR_PIXEL_DEVICES, "Factory Images for Pixel Phones, Tablets"),
+                self.linksMenuItem10.GetId(): (FULL_OTA_IMAGES_FOR_WATCH_DEVICES, "Full OTA Images for Pixel Watches"),
+                self.linksMenuItem11.GetId(): (FACTORY_IMAGES_FOR_WATCH_DEVICES, "Factory Images for Pixel Watches"),
+                self.linksMenuItem12.GetId(): (FULL_OTA_IMAGES_FOR_BETA, "Full OTA Images for Pixel Beta 15"),
+                self.linksMenuItem13.GetId(): (FACTORY_IMAGES_FOR_BETA, "Factory Images for Pixel Beta 15"),
+                self.linksMenuItem14.GetId(): ('https://github.com/5ec1cff/TrickyStore', "5ec1cff's TrickyStore"),
+                self.linksMenuItem15.GetId(): ('https://xdaforums.com/t/pif-faq.4653307/', "osm0sis's PIF FAQ"),
+                self.linksMenuItem16.GetId(): ('https://xdaforums.com/t/info-play-integrity-api-replacement-for-safetynet.4479337/', "V0latyle\'s PI API Info"),
+                self.linksMenuItem17.GetId(): ('https://xdaforums.com/t/module-play-integrity-fix-safetynet-fix.4607985/', "chiteroman's PlayIntegrityFix"),
+                self.linksMenuItem18.GetId(): ('https://xdaforums.com/t/tricky-store-bootloader-keybox-spoofing.4683446/', "Tricky Store (Support Thread)"),
             }
 
             if clicked_id in link_info:
